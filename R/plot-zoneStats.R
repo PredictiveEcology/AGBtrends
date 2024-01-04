43d1b00ada51353900e56d0e2d156e572d8c4a23
Alex Chubaty <achubaty@for-cast.ca>
2024-01-04 14:58:45 -0700

    improve documentation + cleanup plotZoneStats() and plotZoneStatsIntervals()

--- R/plot-zoneStats.R
+++ R/plot-zoneStats.R
@@ -1,14 +1,28 @@
-#' Plot summary statistics
+#' Plot zone summary statistics
 #'
-#' @param file2plot TODO
-#' @param weighted TODO
-#' @param out TODO
+#' @param files2plot character vector of file paths to `.rds` files from which to load
+#'                   a previously-built summary `data.frame` object to use for constructing plots
 #'
-#' @return TODO
+#' @param weighted logical indicating whether to use weighted mean and sd
+#'
+#' @param xVar character string indicating which variable to plot on the x-axis
+#'
+#' @param groupVar character string indicating the variable to group by
+#'
+#' @param catVar character string indicating the category variable
+#'
+#' @param ptype integer indicating plot type: 1 is TODO; 2 is TODO
+#'
+#' @param plotResult logical indicating whether to plot to the active graphics device
+#'
+#' @return `ggplot` object
 #'
 #' @export
-plotZoneStats <- function(file2plot = NULL, weighted = TRUE, out = 1) {
-  sumtab <- readRDS(file2plot) |>
+#' @rdname plotZoneStats
+plotZoneStats <- function(files2plot = NULL, weighted = TRUE, plotResult = TRUE) {
+  stopifnot(length(files2plot) > 1)
+
+  sumtab <- readRDS(files2plot) |>
     mutate(ageClass = factor(ageClass, levels = unique(ageClass), ordered = TRUE))
   catvar <- names(sumtab)[4]
 
@@ -19,8 +33,8 @@ plotZoneStats <- function(file2plot = NULL, weighted = TRUE, out = 1) {
     yr2 = c(1988, 1993, 1998, 2003, 2008, 2014)
   )
 
-  tp <- ifelse(str_detect(file2plot, "_t"),
-               paste0("(", str_c(filter(tref, tp == str_sub(file2plot, start = -6L, end = -5L)) |>
+  tp <- ifelse(str_detect(files2plot, "_t"),
+               paste0("(", str_c(filter(tref, tp == str_sub(files2plot, start = -6L, end = -5L)) |>
                                    select(yr1, yr2) |> unlist(), collapse = "-"), ")"),
                "(1984-2014)"
   )
@@ -45,30 +59,15 @@ plotZoneStats <- function(file2plot = NULL, weighted = TRUE, out = 1) {
                   width = 0.2
     )
 
-  if (out == 1) plot(gp) else return(gp)
+  if (isTRUE(plotResult)) {
+    plot(gp)
+  }
+
+  return(invisible(gp))
 }
 
-#' Plot zone stats intervals
-#'
-#' TODO DESCRIPTION NEEDED
-#'
-#' @param files2plot TODO
-#'
-#' @param weighted TODO
-#'
-#' @param xVar TODO
-#'
-#' @param groupVar TODO
-#'
-#' @param catVar TODO
-#'
-#' @param ptype integer indicating plot type: 1 is TODO; 2 is TODO
-#'
-#' @param plotResult TODO
-#'
-#' @return TODO
-#'
 #' @export
+#' @rdname plotZoneStats
 plotZoneStatsIntervals <- function(files2plot = NULL, weighted = TRUE, xVar = "ageClass",
                                    groupVar = "tp", catVar = NULL, ptype = 1, plotResult = TRUE) {
   ## compile summary tables
@@ -76,8 +75,7 @@ plotZoneStatsIntervals <- function(files2plot = NULL, weighted = TRUE, xVar = "a
     y <- readRDS(x) |>
       mutate(ageClass = factor(ageClass, levels = unique(ageClass), ordered = TRUE))
     y$tp <- factor(rep(str_sub(x, start = -6L, end = -5L), nrow(y)),
-                   levels = paste0("t", 1:6), ordered = TRUE
-    )
+                   levels = paste0("t", 1:6), ordered = TRUE)
     return(y |> relocate(tp, .before = value))
   }))
 
